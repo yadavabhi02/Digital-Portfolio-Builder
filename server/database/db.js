b7f86ceedd3dea2b1520a9a331812e17166f65e3
yadavabhi02 <abhishek02082002@gmail.com>
2024-11-26 08:21:07 +0530

    Initial Commit

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@cluster0.xxb75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+
+export default Connection;
